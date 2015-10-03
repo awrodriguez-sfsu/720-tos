c3022fa4d6fadeda818acba80d25454b2a87729d
Anthony Rodriguez <arodr101@mail.sfsu.edu>
2015-10-03 12:19:45 -0700

    stdlib tests pass and added that weird executable to gitignore

--- kernel/stdlib.c
+++ kernel/stdlib.c
@@ -1,16 +1,53 @@
 
 #include <kernel.h>
 
+int k_strlen(const char* str) {
+    char* string = str;
 
-int k_strlen(const char* str)
-{
+    for(string; *string; string++);
+
+    /* difference in contiguous memory addresses */
+    return string - str;
 }
 
-void* k_memcpy(void* dst, const void* src, int len)
-{
+void* k_memcpy(void* dst, const void* src, int len) {
+    char* destination = (char*) dst;
+    char* source = (char*) src;
+
+    if(destination == source) {
+        return dst;
+    }
+
+    while(len > 0) {
+        *destination = *source;
+        destination++;
+        source++;
+
+        len--;
+    }
+
+    return dst;
 }
 
-int k_memcmp(const void* b1, const void* b2, int len)
-{
+int k_memcmp(const void* b1, const void* b2, int len) {
+    const char* block1 = (char*) b1;
+    const char* block2 = (char*) b2;
+
+    if(block1 == block2) {
+        return 0;
+    }
+
+    while(len > 0) {
+        if(*block1 != *block2) {
+            return *block1 - *block2;
+        }
+
+        block1++;
+        block2++;
+
+        len--;
+    }
+
+    return 0;
 }
 

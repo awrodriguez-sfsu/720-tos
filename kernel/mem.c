45f2c951d6387d6d3aad606508909905103da828
Anthony Rodriguez <arodr101@mail.sfsu.edu>
2015-10-03 12:29:14 -0700

    memory test 1 passes, and I added the tos.img to gitignore that is built when you make tests

--- kernel/mem.c
+++ kernel/mem.c
@@ -1,29 +1,33 @@
-
 #include <kernel.h>
 
+void poke_b (MEM_ADDR addr, BYTE value) {
+    BYTE* dest = (BYTE*) addr;
 
-void poke_b (MEM_ADDR addr, BYTE value)
-{
+    *dest = value;
 }
 
-void poke_w (MEM_ADDR addr, WORD value)
-{
+void poke_w (MEM_ADDR addr, WORD value) {
+    WORD* dest = (WORD*) addr;
+
+    *dest = value;
 }
 
-void poke_l (MEM_ADDR addr, LONG value)
-{
+void poke_l (MEM_ADDR addr, LONG value) {
+    LONG* dest = (LONG*) addr;
+
+    *dest = value;
 }
 
-BYTE peek_b (MEM_ADDR addr)
-{
+BYTE peek_b (MEM_ADDR addr) {
+    return * (BYTE*) addr;
 }
 
-WORD peek_w (MEM_ADDR addr)
-{
+WORD peek_w (MEM_ADDR addr) {
+    return * (WORD*) addr;
 }
 
-LONG peek_l (MEM_ADDR addr)
-{
+LONG peek_l (MEM_ADDR addr) {
+    return * (LONG*) addr;
 }
 
 

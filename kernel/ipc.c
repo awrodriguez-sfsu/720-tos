f683b540a600e977398aeb4e9ab47f2f4b995286
Anthony Rodriguez <arodr101@mail.sfsu.edu>
2015-11-05 09:14:22 -0800

    formatting changes

--- kernel/ipc.c
+++ kernel/ipc.c
@@ -1,54 +1,38 @@
-
 #include <kernel.h>
+#include "../include/kernel.h"
 
+PORT create_port() {
 
-
-
-
-PORT create_port()
-{
 }
 
+PORT create_new_port (PROCESS owner) {
 
-PORT create_new_port (PROCESS owner)
-{
 }
 
+void open_port (PORT port) {
 
-
-
-void open_port (PORT port)
-{
 }
 
+void close_port (PORT port) {
 
-
-void close_port (PORT port)
-{
 }
 
+void send (PORT dest_port, void* data) {
 
-void send (PORT dest_port, void* data)
-{
 }
 
+void message (PORT dest_port, void* data) {
 
-void message (PORT dest_port, void* data)
-{
 }
 
+void* receive (PROCESS* sender) {
 
-
-void* receive (PROCESS* sender)
-{
 }
 
+void reply (PROCESS sender) {
 
-void reply (PROCESS sender)
-{
 }
 
+void init_ipc() {
 
-void init_ipc()
-{
 }
